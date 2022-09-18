095c4d531c970ee19c180adbbdce655c97a5dabf
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-18 10:35:42 -0300

    :sparkles: feat: Adicionando /Cadastro nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import CartProvider from './context/cart';
 import './global.css';
+import { Cadastro } from './pages/Cadastro';
 import { Home } from './pages/Home';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <CartProvider>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
       </Routes>
     </CartProvider>
   );
