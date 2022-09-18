c4649c15866fc60936515c47687c059198d1c78a
juliazib <julia.zibordi@gmail.com>
2022-09-18 15:37:29 -0300

    :sparkles: feat: add Bebidas page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import CartProvider from './context/cart';
 import './global.css';
 import { Aperitivos } from './pages/Aperitivos';
+import { Bebidas } from './pages/Bebidas';
 import { Burguers } from './pages/Burguers';
 import { Cadastro } from './pages/Cadastro';
 import { Home } from './pages/Home';
@@ -16,6 +17,7 @@ function App() {
         <Route path="/Burguers" element={<Burguers />} />
         <Route path="/Aperitivos" element={<Aperitivos />} />
         <Route path="/Sobremesas" element={<Sobremesas />} />
+        <Route path="/Bebidas" element={<Bebidas />} />
         <Route path="/Cadastro" element={<Cadastro />} />
       </Routes>
     </CartProvider>
