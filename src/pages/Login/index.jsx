d3d9fd9aeb4acf054b96bcc648a9bb097df1b39e
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-09-18 18:45:28 -0300

    :sparkles: feat: Criando pagina de login

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,41 @@
+import { LoginStyle, StyledLink } from './style';
+import { Button } from '../../components/Button/style';
+import { InputLabel } from '../../components/InputLabel';
+import teste from '../../assets/google.svg';
+
+export function Login() {
+  return (
+
+    <LoginStyle>
+
+      <div className="container">
+        <div className="imageContainer">
+          <img src="./src/assets/burgerLogin.png" alt="" />
+        </div>
+
+        <div className="loginContainer">
+          <form action="" className="formContainer">
+            <h2 className="title">Bem vindo de volta</h2>
+            <h1 className="title">Faça login na sua conta</h1>
+            <InputLabel labelName="E-mail" placeholderName="Joãodasilva@gmail.com" inputWidth={350} inputHeight={50} required="true" gap={4} />
+            <InputLabel labelName="Senha" placeholderName="Digite sua senha" inputWidth={350} inputHeight={50} required="true" gap={4} />
+            <Button className="logIn" width={347} height={50}>Entrar</Button>
+
+            <Button className="logInGoogle" width={347} height={50}>
+              <img src={teste} alt="" />
+              Ou faça login com o google
+            </Button>
+
+            <div className="cadastro">
+              <span>Não tem uma conta?</span>
+              <StyledLink to="/Cadastro">Faça Cadastro!</StyledLink>
+            </div>
+          </form>
+        </div>
+
+      </div>
+
+    </LoginStyle>
+
+  );
+}
