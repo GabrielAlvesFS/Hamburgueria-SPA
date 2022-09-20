bd4bd669ebc3d1701d4652380986ad4c9de2b669
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-20 12:06:37 -0300

    :sparkles: feat: Adicionando propriedade

--- src/components/InputLabel/index.jsx
+++ src/components/InputLabel/index.jsx
@@ -1,15 +1,15 @@
 import { InputLabelStyle } from './style';
 
 export function InputLabel({
-  labelName, placeholderName, inputWidth, inputHeight, inputType, required, gap,
+  labelName, placeholderName, inputWidth, inputHeight, inputType, required, gap, value, onChange,
 }) {
   return (
     <InputLabelStyle inputWidth={inputWidth} inputHeight={inputHeight} gap={gap}>
       <label htmlFor="input">{labelName}</label>
       {
         required === 'true'
-          ? <input type={inputType} id="input" placeholder={placeholderName} required />
-          : <input type={inputType} id="input" placeholder={placeholderName} />
+          ? <input onChange={onChange} value={value} type={inputType} id="input" placeholder={placeholderName} required />
+          : <input onChange={onChange} value={value} type={inputType} id="input" placeholder={placeholderName} />
       }
     </InputLabelStyle>
   );
