ecc1d0a58872948c0eb2a978589f73ee9e121574
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-09-21 00:55:00 -0300

    :sparkles: feat: metodo put e delete com axios

--- src/services/pedidosApi.js
+++ src/services/pedidosApi.js
@@ -17,13 +17,43 @@ export async function cadastrarItemPedido(reqBody) {
     const res = await api.post('/itemPedido', reqBody, {
       headers: { 'Content-Type': 'application/json' },
     });
-    console.log(res.data);
     return res.data;
   } catch (error) {
     console.log(error);
     return error;
   }
 }
+
+export async function alterarItemPedido(reqBody, pedidoId, itemPedido) {
+  try {
+    const res = await api.put(`/itemPedido/${pedidoId}/${itemPedido}`, reqBody, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+export async function deleteItemPedido(pedidoId, itemPedidoId) {
+  try {
+    const res = await api.delete(`/itemPedido/${pedidoId}/${itemPedidoId}`, {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+// const itemPedidoPutBody = {
+//   item_id: 11,
+//   quantidade_itens: 9999999999,
+// };
+
+// alterarItemPedido(itemPedidoPutBody, 8, 57);
 
 // const itemPedidoBody = {
 //   pedido_id: 4,
