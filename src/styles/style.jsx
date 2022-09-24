fbd1e20c544cfa756b5cd8c15cba43db043d35a2
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-09-24 02:35:42 -0300

    📱 fix: Arrumando responsividade para telas menores

--- src/styles/style.jsx
+++ src/styles/style.jsx
@@ -6,5 +6,20 @@ export const PageContainer = styled.div`
 
   @media ${device.laptop}{
     flex-direction:column;
+    .returnPage {
+  position: absolute;
+  width: 200px;
+  height: 40px;
+  left: 60px;
+  top: 20px;
+
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 36px;
+  color: black;
+
+}
   }
 `;
