7182fa2419d91eecb787a510158a8a5e18c7b789
Gabriel Alves <Gabriel_Alves64@hotmail.com>
2023-02-05 15:11:28 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://hamburguer-api.herokuapp.com/',
+  baseURL: 'https://hamburgueria-api.up.railway.app/',
 });
 
 export default api;
