eee34ffa56274e06be1d3ad78817dfb739c88bd2
Gabriel Alves <Gabriel_Alves64@hotmail.com>
2023-08-09 08:13:46 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://hamburgueria-api.up.railway.app/',
+  baseURL: 'https://hamburgueria-rest-api.cyclic.app/v1',
 });
 
 export default api;
